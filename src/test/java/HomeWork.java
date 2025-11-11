4f6733293ffdee4d555a86790de7e8a026b10dc7
Maria Z <z0559882272@gmail.com>
2025-11-11 20:19:27 +0200

    added xPath

--- src/test/java/HomeWork.java
+++ src/test/java/HomeWork.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 
 public class HomeWork {
 
@@ -40,10 +42,12 @@ public class HomeWork {
 
         WebElement button = wd.findElement(By.tagName("button"));
         WebElement button1 = wd.findElement(By.cssSelector("button"));
+        WebElement button2 = wd.findElement(By.xpath("//button"));
 
         //by class
         WebElement el = wd.findElement(By.className("container"));
         WebElement el1 = wd.findElement(By.cssSelector(".container"));
+        WebElement el15 = wd.findElement(By.xpath("//*[@class='container']"));
 
         WebElement el2 = wd.findElement(By.className("navbar-component_nav__1X_4m"));
         WebElement el3 = wd.findElement(By.cssSelector(".navbar-component_nav__1X_4m"));
@@ -55,6 +59,7 @@ public class HomeWork {
         //by id
         WebElement el6 = wd.findElement(By.id("root"));
         WebElement el7 = wd.findElement(By.cssSelector("#root"));
+        WebElement el14 = wd.findElement(By.xpath("//*[@id='root']"));
 
         //by attribute
         WebElement el8 = wd.findElement(By.cssSelector("[href='/home']"));
@@ -72,6 +77,45 @@ public class HomeWork {
         //contains
         WebElement el13 = wd.findElement(By.cssSelector("[placeholder *='ma']"));
 
+
+        WebElement password = wd.findElement(By.cssSelector("[placeholder='Password]"));
+        WebElement password_xP = wd.findElement(By.xpath("//input[@placeholder='Password']"));
+
+        WebElement password1 = wd.findElement(By.cssSelector("[placeholder ^='Pa']"));
+        WebElement password_xP1= wd.findElement(By.xpath("//input[starts-with(@placeholder,'Pa')]"));
+
+        WebElement password2 = wd.findElement(By.cssSelector("[placeholder *= 'ss']"));
+        WebElement password_xP2 = wd.findElement(By.xpath("//*[contains(@placeholder,'ss')]"));
+
+        WebElement password3 = wd.findElement(By.cssSelector("[placeholder $= 'rd]"));
+        WebElement password_xP3 = wd.findElement(By.xpath("//*[contains(@placeholder,'rd')]"));
+
+    }
+
+
+    @Test
+    public void classwork(){
+        //parent
+        WebElement el = wd.findElement(By.xpath("//h1/.."));
+        WebElement el1 = wd.findElement(By.xpath("//h1/parent::*"));
+        WebElement el2 = wd.findElement(By.xpath("//h1/parent::div"));
+
+
+        //ancestor
+        WebElement el3 = wd.findElement(By.xpath("//h1/ancestor::*"));//all
+        WebElement el4 = wd.findElement(By.xpath("//h1/ancestor::div"));//two options
+        WebElement el5 = wd.findElement(By.xpath("//h1/ancestor::div[2]"));//one option
+
+        //ancestor-or-self
+        WebElement el6 = wd.findElement(By.xpath("//h1/ancestor-or-self::*"));
+        List<WebElement> list = wd.findElements(By.xpath("//h1/ancestor-or-self::*"));
+
+        //following-sibling
+        List<WebElement>list1 = wd.findElements(By.xpath("//h1/following-sibling::a"));
+
+        //preceding-sibling
+        WebElement el7 = wd.findElement(By.xpath("//a[last()]/preceding-sibling::*"));
+        WebElement el8 = wd.findElement(By.xpath("//a[3]/preceding-sibling::a[1]"));
 
     }
 
